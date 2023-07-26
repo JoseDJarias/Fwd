e59f60f4d7df80563c0e8a4a67ed2a85c4d885e5
JoseDJarias <arias_da044@hotmail.com>
2023-07-26 10:09:33 -0600

    addItem to the list functionallity

--- TodoListProyect/src/js/main.js
+++ TodoListProyect/src/js/main.js
@@ -4,23 +4,22 @@ const container = document.createElement('todoList-container')
 const inputItems = document.createElement('div-items');
 const buttonAddItem = document.createElement('btn-addTask');
 const listContainer =document.createElement('list-container');
-const list = document.createElement('list')
- 
+const ul = document.createElement('ul');
+// const item = document.createElement('item')
 
-
-inputItems.innerHTML = `<input type="text" placeholder="Write a task!" id="items">`;
+//innerHTML
+inputItems.innerHTML = `<input type="text" placeholder="Write a task!" id="inputItem">`;
 buttonAddItem.innerHTML = '<button>Add Items</button>';
 
-// add class list
-container.classList.add('todoList-container')
-list.classList.add('list')
-inputItems.classList.add('div-items');
-buttonAddItem.classList.add('buttonAddItem');
-listContainer.classList.add('list-container');
+// add class name
+container.className = "todoList-container"
+inputItems.className ="div-items"
+buttonAddItem.className="buttonAddItem"
+listContainer.className ="list-container"
+ul.className = "ul-list"
 
-
-
-// console.log(typeof listContainer.textContent);
+// append element "ul" to the listContainer
+listContainer.appendChild(ul);
 
 // append elements to the container
 container.appendChild(inputItems);
@@ -31,38 +30,61 @@ container.appendChild(listContainer);
 document.body.appendChild(container);
 
 // Events---------------------------------------------------------------------------------------------------------------------
-buttonAddItem.addEventListener("click", addItems)
+buttonAddItem.addEventListener("click", addItem2)
 
 // functions -----------------------------------------------------------------------------------------------------------------
-function validationInput(){
-  const inputBox = document.getElementById('items').value;
+
+function validationInput() {
+  const inputBox = document.getElementById('inputItem').value;
   if (!inputBox ) {
     alert('Empty Value')
+    return true
   }else if(!isNaN(inputBox) ){
     alert('Not a String')
+    return true
   }
+  return false
   // console.log('Valor: ', inputBox)
 }
 
-function addItems() {
-  const item = document.getElementById('items').value;
+// function addItems() {
+//   const inputBox = document.getElementById('inputItem').value;
+//   if (!validationInput()) {
+//     const prueba =
+//     `<ul class="list-group">
+//         <li class="list-group-item">
+//         <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio"  >
+//         <label >${inputBox} </label>
+//         <button type="button" class="btn btn-outline-warning">Warning</button>
+//         <button type="button" class="btn btn-outline-danger">Danger</button>
+//         </li>
+//      </ul>`; 
+//      console.log(prueba.textContent);
+//      item.innerHTML = prueba;
+     
+//      item.classList.add('item');
   
-  validationInput();
-  
-  list.innerHTML =
-  `<ul class="list-group">
-      <li class="list-group-item">
-      <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio"  >
-      <label >${item} </label>
-      <button type="button" class="btn btn-outline-warning">Warning</button>
-      <button type="button" class="btn btn-outline-danger">Danger</button>
-      </li>
-   </ul>`; 
-   list.classList.add('list');
-   //apend to the container list
-   listContainer.appendChild(list);
+//      //apend to the container list
+//      console.log(item);
+//      listContainer.appendChild(item.textContent);    
+//     }
     
+//   }
+  
+  function addItem2(){
+    const inputBox = document.getElementById('inputItem').value;
+    if (!validationInput()) {
+        const newItem = document.createElement('li');
+        newItem.innerHTML =   
+        `<li class="list-group-item">
+        <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio"  >
+        <label >${inputBox} </label>
+        <button type="button" class="btn btn-outline-warning">Warning</button>
+        <button type="button" class="btn btn-outline-danger">Danger</button>
+        </li>`;
+        ul.appendChild(newItem);        
+      }
     }
-    
+
   
   
