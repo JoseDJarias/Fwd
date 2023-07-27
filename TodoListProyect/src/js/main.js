b33a09b0dd9ae0691f4c47e981fef5283e7dc0f4
JoseDJarias <arias_da044@hotmail.com>
2023-07-27 15:27:05 -0600

    comenting the code

--- TodoListProyect/src/js/main.js
+++ TodoListProyect/src/js/main.js
@@ -1,15 +1,12 @@
 //Global variables-----------------------------------------------------------------------------------------------------
-// const title = document.createElement('title')
 const container = document.createElement('todoList-container')
 const inputItems = document.createElement('div-items');
 const buttonAddItem = document.createElement('btn-addTask');
-const listContainer =document.createElement('list-container');
+const listContainer = document.createElement('list-container');
 const ul = document.createElement('ul');
 const btnClearItems = document.createElement('clearItems');
 const counterBox = document.createElement('counter-box')
 const firstBox = document.createElement('div')
-// const item = document.createElement('item')
-
 // Global variables ----------------------------------------------------------------------------------------------------
 
 //innerHTML
@@ -21,11 +18,11 @@ counterBox.innerHTML = `<span>Tareas Completadas</span> <p>0</p>`
 // add class name
 container.className = "todoList-container"
 firstBox.className = "first-box"
-inputItems.className ="div-items"
-buttonAddItem.className="buttonAddItem"
-counterBox.className ='counter-box';
-btnClearItems.className ="clear-items"
-listContainer.className ="list-container"
+inputItems.className = "div-items"
+buttonAddItem.className = "buttonAddItem"
+counterBox.className = 'counter-box';
+btnClearItems.className = "clear-items"
+listContainer.className = "list-container"
 ul.className = "ul-list"
 
 // append elements such as inputItems, buttonAddItems and counter to the first box
@@ -44,86 +41,92 @@ container.appendChild(listContainer);
 //append elements to body
 document.body.appendChild(container);
 
-// 
+// hide the list container where its created in the dom(and also when a user clear the items list),
+//  and just visible when a user enter a task
 listContainer.style.visibility = "hidden";
 
 // Events---------------------------------------------------------------------------------------------------------------------
 buttonAddItem.addEventListener("click", addItem2);
-btnClearItems.addEventListener('click',clearList)
+btnClearItems.addEventListener('click', clearList)
 // buttonEditItem.addEventListener("click", editItem);
 // Events---------------------------------------------------------------------------------------------------------------------
-
 
 // functions -----------------------------------------------------------------------------------------------------------------
 function validationInput() {
   const inputBox = document.getElementById('inputItem').value;
-  if (!inputBox ) {
+  if (!inputBox) {
     alert('Empty Value')
     return true
-  }else if(!isNaN(inputBox) ){
+  } else if (!isNaN(inputBox)) {
     alert('Not a String')
     return true
   }
   return false
 }
+// add item when a user press click in the buttonAddItem and append to a dinamic list wich also creates the button 
+// for edit and delete and the checkbox for the counter
+function addItem2() {
+  if (!validationInput()) {
+    // Global variables 
+    const inputBox = document.getElementById('inputItem').value;
+    // const newItem = document.createElement('li');
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    const btnEdit = document.createElement('button');
+    const btnDelete = document.createElement('button');
+    const checkbox = document.createElement('input');
 
-  function addItem2( ){
-    if (!validationInput()) {
-      const inputBox = document.getElementById('inputItem').value;
-      const newItem = document.createElement('li');
-      const div = document.createElement('div');
-      const p = document.createElement('p');
-      const btnEdit = document.createElement('button');
-      const btnDelete = document.createElement('button');
-      const checkbox = document.createElement('input');
-      
-      p.textContent = inputBox;
-      btnEdit.textContent = 'Edit';
-      btnDelete.textContent = 'Delete';
-      newItem.innerHTML = `<li class="list-group-item"></li>`;
-      btnEdit.type = 'checkbox';
-      btnDelete.type = 'checkbox';
-      checkbox.type ='checkbox';
-      
+  
+    p.textContent = inputBox;
+    btnEdit.textContent = 'Edit';
+    btnDelete.textContent = 'Delete';
+    // newItem.innerHTML = `<li></li>`;
 
-        btnEdit.className = 'btnBox';
-        btnDelete.className ='btnBox'
-        div.className = 'div-box'
-        p.className = 'item-p'
-        checkbox.className ='cbo'
+    // type checkbox for the buttons
+    btnEdit.type = 'checkbox';
+    btnDelete.type = 'checkbox';
+    checkbox.type = 'checkbox';
 
-        // append elements to the div
-        div.appendChild(checkbox)
-        div.appendChild(newItem)
-        div.appendChild(p);
-        div.appendChild(btnEdit);
-        div.appendChild(btnDelete);
-        // append element div to the ul
-        ul.appendChild(div);
+    // adding class name to the elements
+    btnEdit.className = 'btnBox';
+    btnDelete.className = 'btnBox'
+    div.className = 'div-box'
+    p.className = 'item-p'
+    checkbox.className = 'cbo'
 
-        // functions
-        btnDelete.addEventListener('click', function(){
-          console.log('click')
-          div.remove();
-        })
-        btnEdit.addEventListener('click',function (e) {
+    // append elements to the div
+    div.appendChild(checkbox)
+    // div.appendChild(newItem)
+    div.appendChild(p);
+    div.appendChild(btnEdit);
+    div.appendChild(btnDelete);
 
-          let edit = prompt("Edita tu tarea aqui!")
-          if (edit != null) {
-            p.textContent = edit;
-            
-          }
-        })
+    // append element div to the ul
+    ul.appendChild(div);
 
-        listContainer.style.visibility = "visible";
+    // functions
+    btnDelete.addEventListener('click', function () {
+      console.log('click')
+      div.remove();
+    })
+    btnEdit.addEventListener('click', function (e) {
+
+      let edit = prompt("Edita tu tarea aqui!")
+      if (edit != null) {
+        p.textContent = edit;
+
       }
-    }
+    })
+    // show de the listContainer
+    listContainer.style.visibility = "visible";
+  }
+}
 
-    function clearList(){
-      ul.textContent = "";      
-      listContainer.style.visibility = "hidden";
-    }
+function clearList() {
+  ul.textContent = "";
+  listContainer.style.visibility = "hidden";
+}
 
     // functions ---------------------------------------------------------------------------------------------------------------------
-    
+
 
